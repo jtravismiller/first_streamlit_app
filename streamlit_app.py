967034e06efee62b2c2797a3e78a026d2fbd788e
jtravismiller <jtravismiller1@jtravism.com>
2023-01-08 13:21:09 -0800

    Update streamlit_app.py
    Capitalized titel

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My parents new healthy diner')
+streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
