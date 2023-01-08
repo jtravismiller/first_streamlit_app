5f52b593ca292019d70c653a80b6d904dcb02692
jtravismiller <jtravismiller1@jtravism.com>
2023-01-08 13:39:43 -0800

    Update streamlit_app.py
    Changed index to Fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 import pandas;
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -11,5 +10,10 @@ streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🍎Build Your Own Fruit Smoothie🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+# Pick list
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# Display fruit list
 streamlit.dataframe(my_fruit_list)
