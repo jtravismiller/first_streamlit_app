e260bda332598906507f6504b6f7c62107ba742c
jtravismiller <jtravismiller1@jtravism.com>
2023-01-09 11:39:37 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas;
-
+import requests;
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -19,3 +19,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display fruit list
 streamlit.dataframe(fruits_to_show)
+
+#New Section to display fruityice api response
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
